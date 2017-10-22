d137dadc3e1775521896652e2f5cf5298feee61c
Anna <anni.victoria@hotmail.com>
2017-10-22 00:56:32 -0200

    Translated to English

--- src/GUI/OperationsFrame.java
+++ src/GUI/OperationsFrame.java
@@ -9,7 +9,7 @@ import javax.swing.JButton;
 
 public class OperationsFrame {
 
-	private JFrame frame;
+	private JFrame frmRegularLanguagesOperations;
 
 	/**
 	 * Launch the application.
@@ -19,7 +19,7 @@ public class OperationsFrame {
 			public void run() {
 				try {
 					OperationsFrame window = new OperationsFrame();
-					window.frame.setVisible(true);
+					window.frmRegularLanguagesOperations.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -38,46 +38,47 @@ public class OperationsFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 450, 270);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmRegularLanguagesOperations = new JFrame();
+		frmRegularLanguagesOperations.setTitle("Regular Languages Operations");
+		frmRegularLanguagesOperations.setResizable(false);
+		frmRegularLanguagesOperations.setBounds(100, 100, 450, 270);
+		frmRegularLanguagesOperations.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmRegularLanguagesOperations.getContentPane().setLayout(null);
 		
 		JComboBox cbOpRL1 = new JComboBox();
 		cbOpRL1.setBounds(12, 55, 127, 24);
-		frame.getContentPane().add(cbOpRL1);
+		frmRegularLanguagesOperations.getContentPane().add(cbOpRL1);
 		
 		JComboBox cbOpRL2 = new JComboBox();
 		cbOpRL2.setBounds(290, 55, 127, 24);
-		frame.getContentPane().add(cbOpRL2);
+		frmRegularLanguagesOperations.getContentPane().add(cbOpRL2);
 		
 		JComboBox cbOpOperations = new JComboBox();
 		cbOpOperations.setBounds(151, 55, 127, 24);
-		frame.getContentPane().add(cbOpOperations);
+		frmRegularLanguagesOperations.getContentPane().add(cbOpOperations);
 		
 		JLabel lbOpSelectRL1 = new JLabel("Select RL 1");
 		lbOpSelectRL1.setBounds(32, 28, 80, 15);
-		frame.getContentPane().add(lbOpSelectRL1);
+		frmRegularLanguagesOperations.getContentPane().add(lbOpSelectRL1);
 		
 		JLabel lbOpSelectOp = new JLabel("Select Operation");
 		lbOpSelectOp.setBounds(151, 28, 120, 15);
-		frame.getContentPane().add(lbOpSelectOp);
+		frmRegularLanguagesOperations.getContentPane().add(lbOpSelectOp);
 		
 		JLabel lbOpSelectRL2 = new JLabel("Select RL 2");
 		lbOpSelectRL2.setBounds(320, 28, 80, 15);
-		frame.getContentPane().add(lbOpSelectRL2);
+		frmRegularLanguagesOperations.getContentPane().add(lbOpSelectRL2);
 		
 		JButton btnOpCancel = new JButton("Cancel");
 		btnOpCancel.setBounds(123, 172, 90, 25);
-		frame.getContentPane().add(btnOpCancel);
+		frmRegularLanguagesOperations.getContentPane().add(btnOpCancel);
 		
 		JButton btnOpSave = new JButton("Save");
 		btnOpSave.setBounds(327, 172, 90, 25);
-		frame.getContentPane().add(btnOpSave);
+		frmRegularLanguagesOperations.getContentPane().add(btnOpSave);
 		
 		JButton btnOpView = new JButton("View");
 		btnOpView.setBounds(225, 172, 90, 25);
-		frame.getContentPane().add(btnOpView);
+		frmRegularLanguagesOperations.getContentPane().add(btnOpView);
 	}
 }
--- src/GUI/ViewEditFrame.java
+++ src/GUI/ViewEditFrame.java
@@ -5,10 +5,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JRadioButton;
 import javax.swing.JEditorPane;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
 public class ViewEditFrame {
 
-	private JFrame frame;
+	private JFrame frmRegularLanguagesOperations;
 
 	/**
 	 * Launch the application.
@@ -18,7 +20,7 @@ public class ViewEditFrame {
 			public void run() {
 				try {
 					ViewEditFrame window = new ViewEditFrame();
-					window.frame.setVisible(true);
+					window.frmRegularLanguagesOperations.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -37,34 +39,44 @@ public class ViewEditFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 500, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmRegularLanguagesOperations = new JFrame();
+		frmRegularLanguagesOperations.setTitle("View and Edit Regular Languages");
+		frmRegularLanguagesOperations.setResizable(false);
+		frmRegularLanguagesOperations.setBounds(100, 100, 500, 500);
+		frmRegularLanguagesOperations.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmRegularLanguagesOperations.getContentPane().setLayout(null);
 		
 		JRadioButton rdbtnViewEditRE = new JRadioButton("Regular Expression");
 		rdbtnViewEditRE.setBounds(8, 77, 165, 23);
-		frame.getContentPane().add(rdbtnViewEditRE);
+		frmRegularLanguagesOperations.getContentPane().add(rdbtnViewEditRE);
 		
 		JRadioButton rdbtnViewEditFA = new JRadioButton("Finite Automata");
 		rdbtnViewEditFA.setBounds(182, 77, 145, 23);
-		frame.getContentPane().add(rdbtnViewEditFA);
+		frmRegularLanguagesOperations.getContentPane().add(rdbtnViewEditFA);
 		
 		JRadioButton rdbtnViewEditRG = new JRadioButton("Regular Grammar");
 		rdbtnViewEditRG.setBounds(330, 77, 170, 23);
-		frame.getContentPane().add(rdbtnViewEditRG);
+		frmRegularLanguagesOperations.getContentPane().add(rdbtnViewEditRG);
 		
-		JEditorPane editorPane = new JEditorPane();
-		editorPane.setBounds(24, 108, 141, 268);
-		frame.getContentPane().add(editorPane);
+		JEditorPane edpViewEditRE = new JEditorPane();
+		edpViewEditRE.setBounds(24, 108, 141, 268);
+		frmRegularLanguagesOperations.getContentPane().add(edpViewEditRE);
 		
-		JEditorPane editorPane_1 = new JEditorPane();
-		editorPane_1.setBounds(176, 108, 141, 268);
-		frame.getContentPane().add(editorPane_1);
+		JEditorPane edpViewEditFA = new JEditorPane();
+		edpViewEditFA.setBounds(176, 108, 141, 268);
+		frmRegularLanguagesOperations.getContentPane().add(edpViewEditFA);
 		
-		JEditorPane editorPane_2 = new JEditorPane();
-		editorPane_2.setBounds(328, 108, 141, 268);
-		frame.getContentPane().add(editorPane_2);
+		JEditorPane edpViewEditRG = new JEditorPane();
+		edpViewEditRG.setBounds(328, 108, 141, 268);
+		frmRegularLanguagesOperations.getContentPane().add(edpViewEditRG);
+		
+		JButton btnViewEditSave = new JButton("Save");
+		btnViewEditSave.setBounds(356, 429, 117, 25);
+		frmRegularLanguagesOperations.getContentPane().add(btnViewEditSave);
+		
+		JButton btnViewEditCancel = new JButton("Cancel");
+		btnViewEditCancel.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnViewEditCancel.setBounds(234, 429, 117, 25);
+		frmRegularLanguagesOperations.getContentPane().add(btnViewEditCancel);
 	}
 }
